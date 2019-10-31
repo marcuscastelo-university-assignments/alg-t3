880a0ee85ef05dff00463bdf9468f775f046e6fe
marcuscastelo <viniciuscastelo6@hotmail.com>
2019-10-31 11:52:51 -0300

    adiciona ordenada

--- colecao.c
+++ colecao.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include "colecao.h"
 
+typedef enum {
+    ID_LISTA_ORDENADA = 1, ID_LISTA_ULTIMA, ID_LISTA_PRIMEIRA, ID_ABB, ID_AVL
+} ESTRUTURA_ID;
+
 struct _no {
     int valor;
     struct _no* esq;
@@ -31,9 +35,51 @@ No* cria_no(int valor)
     return no;
 }
 
+void _adiciona_ordenada(Colecao *c, int valor) {
+    if (c->inicio == NULL) {
+        c -> inicio = cria_no(valor);
+        return;
+    }
+
+    if (c -> inicio -> valor > valor) {
+        c -> inicio -> esq = cria_no(valor);
+        c -> inicio -> esq -> dir = c -> inicio;
+        c -> inicio = c -> inicio -> esq; 
+        return;
+    }
+
+    #define noNavegadorDir (noNavegador->dir)
+
+    No *noNavegador = c -> inicio;
+    while (noNavegadorDir != NULL && noNavegadorDir -> valor < valor) noNavegador = noNavegadorDir;
+    No *novoNo = cria_no(valor);
+
+    novoNo -> dir = noNavegadorDir;
+    novoNo -> esq = noNavegador;
+
+    novoNo -> dir -> esq = novoNo;
+    novoNo -> esq -> dir = novoNo;
+}
+
+void _adiciona_inicio(Colecao *c, int valor) {
+
+}
+
+void _adiciona_fim(Colecao *c, int valor) {
+
+}
+
 void adiciona(Colecao* c, int valor)
 {
-    // Implementar
+    switch (c -> estrutura_id)
+    {
+    case ID_LISTA_ORDENADA:
+        _adiciona_ordenada(c, valor);
+        break;
+    
+    default:
+        break;
+    }
 }
 
 int existe(Colecao* c, int valor)
